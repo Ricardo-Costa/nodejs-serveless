8a98b9f193399ef774b0cd880604b87fb6941b2a
Ricardo Costa <ricardo18acs@gmail.com>
2022-02-13 13:43:50 -0300

    feat: add tools to test in dev stage

--- README.md
+++ README.md
@@ -14,7 +14,27 @@ npm install -g serverless
 serverless create --template aws-nodejs --path nodejs-serveless
 ```
 
+**Test function with servelesss invoke**
+
+```bash
+serverless invoke local --function hello --path mocks/test.json --region us-east-1
+```
+
+**Start OFF line**
+
+```bash
+serverless offline
+```
+
+**Run test offline with aws CLI**
+
+```bash
+aws lambda invoke /dev/null --endpoint-url http://localhost:3002 --function-name hello
+```
+
 **References**
 
 - https://www.serverless.com/framework/docs/getting-started
 - http://slss.io/aws-creds-setup
+- https://www.serverless.com/plugins/serverless-offline
+- https://docs.aws.amazon.com/pt_br/cli/latest/userguide/install-linux.html
--- handler.js
+++ handler.js
@@ -1,11 +1,33 @@
-'use strict';
+"use strict";
+
+const { Lambda } = require("aws-sdk");
+
+exports.handler = async function () {
+  const lambda = new Lambda({
+    apiVersion: "2015-03-31",
+    // endpoint needs to be set only if it deviates from the default, e.g. in a dev environment
+    // process.env.SOME_VARIABLE could be set in e.g. serverless.yml for provider.environment or function.environment
+    endpoint: process.env.SOME_VARIABLE
+      ? "http://localhost:3002"
+      : "https://lambda.us-east-1.amazonaws.com",
+  });
+
+  const params = {
+    // FunctionName is composed of: service name - stage - function name, e.g.
+    FunctionName: "hello",
+    InvocationType: "RequestResponse",
+    Payload: JSON.stringify({ data: "foo" }),
+  };
+
+  const response = await lambda.invoke(params).promise();
+};
 
 module.exports.hello = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: "Go Serverless v1.0! Your function executed successfully!",
         input: event,
       },
       null,
